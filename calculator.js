2e45ff79129b47e8c09d75b4f2bb48f2dd0d4fab
Ola Ishola <45001916+netman5@users.noreply.github.com>
2022-01-18 13:06:43 +0100

    Add calculator class & methods

--- calculator.js
+++ calculator.js
@@ -1,22 +1,17 @@
 class Calculator {
-  //   constructor(add, subtract, multiply, divide) {
-  //     this.add = add;
-  //     this.subtract = subtract;
-  //     this.multiply = multiply;
-  //     this.divide = divide;
-  //   }
-
-  add(x, y) {
+  static add(x, y) {
     return x + y;
   }
 
-  multiply = (x, y) => x * y;
+  static multiply = (x, y) => x * y;
 
-  subtract(x, y) {
+  static subtract(x, y) {
     return x - y;
   }
 
-  divide = (x, y) => x / y;
+  static divide = (x, y) => x / y;
 }
+
+console.log(Calculator.multiply(4, 2));
 
 module.exports = Calculator;
--- test-practice.test.js
+++ test-practice.test.js
@@ -1,22 +1,10 @@
 const stringLength = require('./test-practice');
 const reverseString = require('./reverse');
-// const Calculator = require('./calculator');
-class Calculator {
-  add(x, y) {
-    return x + y;
-  }
-
-  multiply = (x, y) => x * y;
-
-  subtract(x, y) {
-    return x - y;
-  }
-
-  divide = (x, y) => x / y;
-}
+const Calculator = require('./calculator');
 
 it('should return string count', () => {
-  expect(stringLength('true')).toBe(4);
+  const string = 'Heloooo';
+  expect(stringLength(string)).toBe(4);
 });
 
 it('String should not be 1 and not longer than 10', () => {
@@ -24,15 +12,39 @@ it('String should not be 1 and not longer than 10', () => {
 });
 
 it('String longer than 10', () => {
-  expect(stringLength('I love this testing')).toBe(19);
+  const testStr = 'I love this testing';
+  expect(stringLength(testStr)).toBe(19);
 });
 
 it('Reverse a string', () => {
-  expect(reverseString('hello')).toMatch(/olleh/);
+  const reverse = 'hello';
+  expect(reverseString(reverse)).toMatch(/olleh/);
 });
 
-describe('Calculator', () => {
-  test('sum x + y', () => {
+describe('Add x + y', () => {
+  test('sum 2, 5', () => {
     expect(Calculator.add(2, 5)).toBe(7);
+  });
+
+  test('sum 10 + 5 to be 15', () => {
+    expect(Calculator.add(5, 15)).toBe(20);
+  });
+
+  test('sum 20 + 5 to be 25', () => {
+    expect(Calculator.add(20, 5)).toBe(25);
+  });
+});
+
+describe('Multiply x * y', () => {
+  test('Multiply 3 * 3 equal 9', () => {
+    expect(Calculator.multiply(3, 3)).toBe(9);
+  });
+
+  test('Multiply 10 * 30 equal 9', () => {
+    expect(Calculator.multiply(10, 30)).toBe(300);
+  });
+
+  test('Multiply 5 * 2 equal 10', () => {
+    expect(Calculator.multiply(5, 2)).toBe(10);
   });
 });
